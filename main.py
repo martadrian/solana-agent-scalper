fcf52a4dc8fb6b0e5ce4a10cd204f7eb24d93bb7
martadrian <164796250+martadrian@users.noreply.github.com>
2026-02-23 23:14:30 +0100

    Remove unnecessary blank line before main execution

--- main.py
+++ main.py
@@ -195,4 +195,3 @@ if __name__ == "__main__":
     loop = asyncio.new_event_loop(); asyncio.set_event_loop(loop)
     try: loop.run_until_complete(main())
     except Exception as e: print(f"Crash: {e}")
-    
